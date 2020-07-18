ec8ca24d9fb6dffc9c71d001eec0fff39a85af0c
ionicstuff <vaibhav.nadgonde1980@gmail.com>
2020-07-18 08:45:12 +0530

    added controller, middleware, routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//calling route file
+const planCalculator = require('./routes/calculators/plan_calculator');
+
 //load env
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/calculators/', planCalculator);
 
 PORT = process.env.PORT || 5000;
 app.listen(
